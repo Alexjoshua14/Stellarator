731dab6d781e9d718f19a8e14eb483ea80e986b8
Alex <ajoshua14@icloud.com>
2024-01-25 22:39:50 -0800

    Refine home button

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -8,7 +8,7 @@ interface TopBarProps {
 const TopBar: FC<TopBarProps> = ({ }) => {
   return (
     <div className="z-40 fixed top-0 left-0 w-full h-20 px-10 flex items-center bg-white/5 backdrop-blur-2xl">
-      <Link href="/">
+      <Link href="/" className="px-4 py-2 rounded-sm text-sm backdrop-brightness-125 hover:bg-white/10 hover:backdrop-contrast-200 hover:backdrop-brightness-100 active:backdrop-brightness-75 transition-all duration-300 ease-in-out">
         {`Home_`}
       </Link>
     </div>
